38a83d46c0d70566a9c75f825383d1d4ed62cedb
freeislet <freeislet@gmail.com>
2024-03-07 13:42:46 +0900

    endpoint 추가 테스트 (function 빌드 파악)

--- api/index.py
+++ api/index.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/api/python2")
+def hello_world():
+    return {"message": "Hello World 2"}
